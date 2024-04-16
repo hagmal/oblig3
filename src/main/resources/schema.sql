6f89512a0fd35e75fc77f227a74d375e9c5ed1f4
hagmal <mahag2193@oslomet.no>
2024-04-16 11:19:14 +0200

    Endret på schema og lagt til spring.datasoruce.url=jdbc:h2:mem:testdb i application.properties for å kunne logge inn på databasen

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE KinoBillett
-{
-    id INTEGER AUTO_INCREMENT NOT NULL,
-    film VARCHAR(50) NOT NULL,
-    antall INTEGER NOT NULL,
-    fornavn VARCHAR (255) NOT NULL,
-    etternavn VARCHAR (255) NOT NULL,
-    telefonnr INTEGER NOT NULL,
-    epost VARCHAR (255) NOT NULL,
+CREATE TABLE Kinobillett
+(
+    id        INTEGER AUTO_INCREMENT NOT NULL,
+    film      VARCHAR(50)  NOT NULL,
+    antall    INTEGER      NOT NULL,
+    fornavn   VARCHAR(255) NOT NULL,
+    etternavn VARCHAR(255) NOT NULL,
+    telefonnr INTEGER      NOT NULL,
+    epost     VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
-}
+);
